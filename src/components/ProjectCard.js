e944c125fa9147de53f3a9956f0bd3614f5061d9
Victor Gou <victorgou.design@gmail.com>
2024-07-08 19:54:46 -0300

    ProjectCard manual path working

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,45 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import './ProjectCard.css';
+// Image imports
+import coverImage from '../assets/projects/futurama/cover.jpg';
+import contentImage from '../assets/projects/futurama/content/1.jpg';
 
-const ProjectCard = ({ coverImage, contentImage, infoText }) => {
-  const [info, setInfo] = useState('');
-  const [showContent, setShowContent] = useState(false);
+const ProjectCard = () => {
+    const [info, setInfo] = useState('');
+    const [showContent, setShowContent] = useState(false);
 
-  useEffect(() => {
-    // Fetch info text
-    fetch(infoText)
-      .then(response => response.text())
-      .then(text => setInfo(text))
-      .catch(error => console.error('Error fetching the info:', error));
-  }, [infoText]);
+    useEffect(() => {
+        fetch(require('../assets/projects/futurama/info.txt'))
+            .then(response => response.text())
+            .then(text => setInfo(text))
+            .catch(error => console.error('Error fetching the info:', error));
+    }, []);
 
-  const handleClick = () => {
-    setShowContent(true);
-  };
+    const handleClick = () => {
+        setShowContent(true);
+    };
 
-  const handleCloseContent = (e) => {
-    e.stopPropagation();
-    setShowContent(false);
-  };
+    const handleCloseContent = (e) => {
+        e.stopPropagation();
+        setShowContent(false);
+    };
 
-  return (
-    <>
-      <div className="profile_card" onClick={handleClick}>
-        <img src={coverImage} alt="cover" />
-        <div className='info'>{info}</div>
-      </div>
-      {showContent && (
-        <div className="content-overlay">
-          <div className="content-container">
-            <button className="close-button" onClick={handleCloseContent}>X</button>
-            <div className='content'>
-              <img src={contentImage} alt="content" />
+    return (
+        <>
+            <div className="profile_card" onClick={handleClick}>
+                <img src={coverImage} alt="cover" />
+                <div className='info'>{info}</div>
             </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+            {showContent && (
+                <div className="content-overlay">
+                    <div className="content-container">
+                        <button className="close-button" onClick={handleCloseContent}>X</button>
+                        <div className='content'>
+                            <img src={contentImage} alt="content" />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default ProjectCard;
--- src/layouts/main/M2.js
+++ src/layouts/main/M2.js
@@ -2,22 +2,12 @@ import React from 'react';
 import './main_grid.css'
 import ProjectCard from '../../components/ProjectCard'
 
-// Static imports
-import coverImage from '../../assets/projects/futurama/cover.jpg';
-import contentImage from '../../assets/projects/futurama/content/1.jpg';
-import infoText from '../../assets/projects/futurama/info.txt';
-
-
-
 
 const M2 = () => {
+
   return (
     <div className="projects">
-      <ProjectCard 
-        coverImage={coverImage}
-        contentImage={contentImage}
-        infoText={infoText}
-      />
+      <ProjectCard />
     </div>
   );
 };
