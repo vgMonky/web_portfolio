7ea469301f367601fd5333789cb2f96d726ea59e
Victor Gou <victorgou.design@gmail.com>
2024-07-21 18:54:14 -0300

    projects doble slide

--- src/components/GridGallery.js
+++ src/components/GridGallery.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './GridGallery.css';
 import ProjectCard from './ProjectCard';
 
-const ProjectSet = () => (
+const ProjectSetR = () => (
   <>
     <ProjectCard path="projects/futurama" />
-    <ProjectCard path="projects/ingenia" />
     <ProjectCard path="projects/futurama" />
     <ProjectCard path="projects/futurama" />
     <ProjectCard path="projects/ingenia" />
+  </>
+);
+
+const ProjectSetL = () => (
+  <>
+    <ProjectCard path="projects/ingenia" />
+    <ProjectCard path="projects/ingenia" />
+    <ProjectCard path="projects/futurama" />
   </>
 );
 
@@ -16,11 +23,15 @@ const GridGallery = () => {
   return (
     <div className="outer-container">
       <div className="grid-container">
-        <div className="grid-track">
-          <ProjectSet />
+        <div className="grid-track grid-track-left">
+          <ProjectSetL />
+          <ProjectSetL />
+          <ProjectSetL />
         </div>
-        <div className="grid-track">
-          <ProjectSet />
+        <div className="grid-track grid-track-right">
+          <ProjectSetR />
+          <ProjectSetR />
+          <ProjectSetR />
         </div>
       </div>
     </div>
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,76 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './ProjectCard.css';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const ProjectCard = ({ path }) => {
-  const [name, setName] = useState('');
-  const [type, setType] = useState('');
-  const [showContent, setShowContent] = useState(false);
-  const [contentImages, setContentImages] = useState([]);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    const loadInfo = async () => {
-      try {
-        const infoModule = await import(`../assets/${path}/info.txt`);
-        const infoText = await fetch(infoModule.default).then(res => res.text());
-        const lines = infoText.split('\n');
-        if (lines.length >= 2) {
-          const nameMatch = lines[0].match(/"(.*?)"/);
-          const typeMatch = lines[1].match(/"(.*?)"/);
-          if (nameMatch) setName(nameMatch[1]);
-          if (typeMatch) setType(typeMatch[1]);
-        } else {
-          console.error('Unexpected format in info.txt');
-        }
-      } catch (error) {
-        console.error('Error loading info:', error);
-      }
-    };
-  
-    loadInfo();
-
-    // Load content images
-    const loadImages = async () => {
-      try {
-        const imageContext = require.context('../assets', true, /\.(png|jpe?g|svg)$/);
-        const imageKeys = imageContext.keys().filter(key => key.includes(`${path}/content/`));
-        const images = imageKeys.map(key => imageContext(key));
-        setContentImages(images);
-      } catch (error) {
-        console.error('Error loading images:', error);
-      }
-    };
-    loadImages();
-  }, [path]);
-
-  const handleClick = () => {
-    setShowContent(true);
-  };
-
-  const handleCloseContent = (e) => {
-    e.stopPropagation();
-    setShowContent(false);
-  };
-
-  const handlePrevImage = (e) => {
-    e.stopPropagation();
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? contentImages.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleNextImage = (e) => {
-    e.stopPropagation();
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === contentImages.length - 1 ? 0 : prevIndex + 1
-    );
-  };
 
   return (
-      <div className="project_card" /*onClick={handleClick}*/>
-        <img src={require(`../assets/${path}/cover.jpg`)} alt="cover" />
-      </div>
+    <div className="project_card">
+      <img src={require(`../assets/${path}/cover.jpg`)} alt="cover" />
+    </div>
   );
 };
 
