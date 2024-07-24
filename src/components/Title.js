3f514ec40e378c15c9e50451f33f0afe204ffb45
Victor Gou <victorgou.design@gmail.com>
2024-07-24 16:52:40 -0300

    sections added in Main, and number component created

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Title.css';
 
-const Title = ({text, text2}) => {
-
+const Title = ({ text, text2, color = 't0' }) => {
   return (
-    <div className="title">
-        <h2>{text}</h2>
-        <h3>{text2}</h3>
+    <div className="title" style={{ color: `var(--${color})` }}>
+      <h2>{text}</h2>
+      <h3>{text2}</h3>
     </div>
   );
 };
 
-export default Title
+export default Title;
--- src/layouts/main/M1.js
+++ src/layouts/main/M1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './M1.css';
+import './M.css';
 import HeroText from '../../components/HeroText';
 import Title from '../../components/Title';
 import ProfileImg from '../../components/ProfileImg';
@@ -9,108 +9,93 @@ import Expand from '../../components/Expand';
 import profile_img from '../../assets/profile.jpg';
 import Slide from '../../components/Slide';
 import ProjectCard from '../../components/ProjectCard';
+import Numbers from '../../components/Numbers';
 
 const M1 = () => {
   return (
     <div className="main-container">
 
-      <div className='row'>
-        <div className='m10'><HeroText text={"Front end Designer with +4 years of experience."}/></div>
-      </div>
-
-      <div className='row'>
-        <div className='m10'><Logos/></div>
-      </div>
-      <div className='row space'>
-      </div>
-
-      <div className='row'>
-        <div className='m3'><ProfileImg src={profile_img} /></div>
-        <div className='m7'>
-          <ProfileCard
-            title="Victor Goudschaal."
-            description1="Digital Creator"
-            description2="Click here to download CV document."
-          />
+      <div className='section'>
+        <div className='row'>
+          <div className='m10'><HeroText /></div>
         </div>
       </div>
 
 
+      <div className='section'>
+        <div className='row'>
+          <div className='m10'><Logos/></div>
+        </div>
 
-
-
-      <div className='row'>
-        <div className='m10'><Title 
-        text={"+4 years creating digital products."}
-        text2={"Expand your bussiness with the power of technology & design."}
-        /></div>
-      </div>
-      <div className='row'>
-        <div className='m10'><Slide dir="right">
-          <ProjectCard path="projects/ombu" />
-          <ProjectCard path="projects/la_despensa" />
-          <ProjectCard path="projects/futurama" />
-          <ProjectCard path="projects/ingenia" />
-        </Slide></div>
-      </div>
-      <div className='row'>
-        <div className='m10'><Slide dir="left">
-          <ProjectCard path="projects/unis" />
-          <ProjectCard path="projects/reinventar" />
-          <ProjectCard path="projects/scrum_go" />
-          <ProjectCard path="projects/skygpu" />
-          <ProjectCard path="projects/sonder" />
-        </Slide></div>
+        <div className='row'>
+          <div className='m3'><ProfileImg src={profile_img} /></div>
+          <div className='m7'>
+            <ProfileCard
+              title="Victor Goudschaal."
+              description1="Digital Creator"
+              description2="Click here to download CV document."
+            />
+          </div>
+        </div>
       </div>
 
 
-
-
-      <div className='row'>
-        <div className='m10'><Title
-        text={"Who is a Front End Designer?"}
-        text2={"Living between the world of communication and engineering:"}
-        /></div>
-      </div>
-      <div className='row'>
-        <div className='m5'><h1>1</h1><h3>they understand the UX principles</h3></div>
-        <div className='m5'><h1>2</h1><h3>they have an eye for aesthetics</h3></div>
-        <div className='m5'><h1>3</h1><h3>they can write HTML, CSS & JavaScript.</h3></div>
-        <div className='m5'><h1>4</h1><h3>they understand the possibilities of frontend development</h3></div>
-      </div>
-
-
-      <div className='row space'>
-      </div>
-      <div className='row space'>
-      </div>
-      <div className='row space'>
-      </div>
-      <div className='row space'>
-      </div>
-
-
-      <div className='row'>
-        <div className='m10'><Title
-        text={"Let’s Connect!"}
-        text2={"Choose a convenient time or drop a message."}
-        /></div>
+      <div className='section'>
+        <div className='row'>
+          <div className='m10'><Title 
+          text={"+4 years creating digital products."}
+          text2={"Expand your bussiness with the power of technology & design."}
+          /></div>
+        </div>
+        <div className='row'>
+          <div className='m10'><Slide dir="right">
+            <ProjectCard path="projects/ombu" />
+            <ProjectCard path="projects/la_despensa" />
+            <ProjectCard path="projects/futurama" />
+            <ProjectCard path="projects/ingenia" />
+          </Slide></div>
+        </div>
+        <div className='row'>
+          <div className='m10'><Slide dir="left">
+            <ProjectCard path="projects/unis" />
+            <ProjectCard path="projects/reinventar" />
+            <ProjectCard path="projects/scrum_go" />
+            <ProjectCard path="projects/skygpu" />
+            <ProjectCard path="projects/sonder" />
+          </Slide></div>
+        </div>
       </div>
 
-      <div className='row'>
-        <div className='m10'><Expand
-          subtitle="Schedule Meeting"
-          description="content..."
-        /></div>
-      </div>
-
-      <div className='row'>
-        <div className='m10'><Expand
-          subtitle="Send Mail" 
-          description="content..."
-        /></div>
+      <div className='space'></div>
+      <div className='section t0'>
+        <div className='row'>
+          <div className='m10'><Title
+          text={"Who is a Front End Designer?"}
+          text2={"Living between the world of communication and engineering:"}
+          color='t3'
+          /></div>
+        </div>
+        <div className='row'>
+          <div className='m10'><Numbers /></div>
+        </div>
       </div>
       
+
+      <div className='section'>
+        <div className='row'>
+          <div className='m10'><Title
+          text={"Let’s Connect! "}
+          text2={"Choose a convenient time or drop a message."}
+          /></div>
+        </div>
+        <div className='row'>
+          <div className='m10'><Expand subtitle={"Schedule Meeting"}/></div>
+        </div>
+        <div className='row'>
+          <div className='m10'><Expand subtitle={"Send Mail"}/></div>
+        </div>
+      </div>
+
     </div>
   );
 };
