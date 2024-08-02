9cd8677a24dbc711c1500201d8c4b68ecb7d8d83
Vik <victorgou.design@gmail.com>
2024-08-01 22:11:57 -0300

    navbar logo added

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -2,15 +2,13 @@
 import React from 'react';
 import Menu from '../components/Menu';
 import './Navbar.css';
+import { ReactComponent as Logo } from '../assets/victor_gou/logo.svg';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className='navbar-content'>
-        <div className="logo hover">
-          <img src="/path/to/logo.png" alt="Logo" />
-          <span>Victor Gou.</span>
-        </div>
+        <Logo width="130" height="50" fill="var(--t3)" />
         <Menu m1={"Home"} m2={"Proyects"} m3={"Contact"}/>
       </div>
     </nav>
